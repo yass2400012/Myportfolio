652cea93836ab910707dcd5d32f54a5555fe78a6
Yassine <yass2400012@gmail.com>
2023-06-01 20:24:16 -0400

    Mobile v2

--- public/js/app.js
+++ public/js/app.js
@@ -161,7 +161,7 @@ ScrollReveal({
 })
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box .contact', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
